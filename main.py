e54386a5555390ed9cc75966e0d84d2d442821d8
Jakob__init__ <jsuetfels@gmail.com>
2024-07-14 20:22:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import pygame 
+import math
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sim")
+
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+class Planet:
+    AU = 149.6e6 * 1000 # Es ist die Strecke der Erde zur Sonne, kann als Einheit verwendet werden
+    G = 6.67428e-11
+    SCALE = 250 / AU #1 Au = c.a 100px
+    TIMESTEP = 3600*24 # 1 day
+    
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+        
+        self.x_vel = 0
+        self.y_vel = 0
+    
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH /  2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+        
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30) # Die echte Masse der Sonne in KG
+    sun.sun = True
+    
+    planets = [sun]
+    
+    while run:
+        clock.tick(60)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+        for planet in planets:
+            planet.draw(WIN)
+        
+        pygame.display.update() 
+            
+    pygame.quit()
+    
+main()
